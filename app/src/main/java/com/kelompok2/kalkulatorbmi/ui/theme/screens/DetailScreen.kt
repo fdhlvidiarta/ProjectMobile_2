8fc858d6f62d0a7647a95e04f5c1b2486e7ffb87
HrsCode11 <hariscahya35@gmail.com>
2024-12-14 11:22:50 +0700

    Perbaikan Tampilan

--- app/src/main/java/com/kelompok2/kalkulatorbmi/ui/theme/screens/DetailScreen.kt
+++ app/src/main/java/com/kelompok2/kalkulatorbmi/ui/theme/screens/DetailScreen.kt
@@ -2,20 +2,26 @@ package com.kelompok2.kalkulatorbmi.ui.theme.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.kelompok2.kalkulatorbmi.R
 
 @Composable
@@ -26,7 +32,6 @@ fun DetailDadaAyamScreen() {
             .background(Color.White) // Latar belakang putih
             .padding(16.dp)
             .verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // Gambar di bagian atas tanpa bayangan
         Image(
